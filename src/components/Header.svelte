39c8edda636ba3df4d0856d65c9dc2f024074897
Kanaiym Abdigaparova <abdigak@clarkson.edu>
2023-03-22 20:48:23 -0400

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+const links = [
+    {'href': '/', 'label': 'Home'},
+    {'href': '/Gallery', 'label': 'Gallery'}
+]
+</script>
+
+<main>
+    <ul>
+        {#each links as link}
+            <li><a href="{link.href}">{link.label}</a></li>
+        {/each}
+    </ul>
+</main>
---
+++ src/routes/Gallery/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Hello COMM429!</h1>
