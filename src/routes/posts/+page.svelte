5d9ffb9a4d18468e681f4db4b126a8364abb4559
Kanaiym Abdigaparova <abdigak@clarkson.edu>
2023-03-30 12:20:44 -0400

    setting up a loop

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -5,3 +5,9 @@
   <h1>{data.post.title}</h1>
   <p>Article #{data.post.slug}</p>
   <p>{data.post.content}</p>
+
+  <nav class="sidebar">
+    {#each data.summaries as {slug, title}}
+      <a href="/posts/{slug}">{title}</a>
+    {/each}
+  </nav>
