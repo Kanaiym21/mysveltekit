10d31d8dc4dc9aa96e738f5ac32f91d630da848f
Kanaiym Abdigaparova <abdigak@clarkson.edu>
2023-04-25 11:17:05 -0400

    adjustments

---
+++ src/routes/Gallery/+page.server.js
@@ -0,0 +1,16 @@
+export async function load({ fetch }) {  
+    const page = Math.floor(Math.random() * 10000) + 1;
+  
+    const url = 'https://api.artic.edu/api/v1/artworks?fields=image_id,title,artist_title,=' + page + '&limit=15';
+  
+    const artReq = await fetch(url);
+    
+    const artRes = await artReq.json();
+  
+    const artworks = artRes.data;
+  
+    return {
+      artworks
+    }
+  
+  }
